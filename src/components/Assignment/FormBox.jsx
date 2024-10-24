28273d5fee9108a25021edd985462a11a74f2932
nyi-nyi-lynn <nyinyilynn163@gmail.com>
2024-10-24 17:41:56 +0630

    Assignment with useRef

--- src/components/Assignment/FormBox.jsx
+++ src/components/Assignment/FormBox.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 
 const FormBox = (props) => {
-  const [name,setName]  = useState("");
-  const [email,setEmail] = useState("");
-  const [live,setLive] = useState("");
+  // const [name,setName]  = useState("");
+  // const [email,setEmail] = useState("");
+  // const [live,setLive] = useState("");
+  const nameInputRef = useRef();
+  const liveInputRef = useRef();
+  const emailInputRef = useRef();
+
 
   const getStuInfo = (event) =>{
     event.preventDefault();  
+    
       if(
-        name.trim().length === 0 ||
-        email.trim().length === 0 ||
-        live.trim().lenght === 0
+        nameInputRef.current.value.trim().length === 0 ||
+        emailInputRef.current.value.trim().length === 0 ||
+        liveInputRef.current.value.trim().lenght === 0
       ){
         alert("please fill a valid value for all inputs")
         return;
       }
       const data = {
-        name:name,
-        email:email,
-        live:live,
+        name:nameInputRef.current.value,
+        email:emailInputRef.current.value,
+        live:liveInputRef.current.value,
       }
       props.addNewStudent(data);
       
-     setName("");
-     setEmail("");
-     setLive("");
+      nameInputRef.current.value = "";
+      liveInputRef.current.value = "";
+      emailInputRef.current.value = "";
+
+   
   } 
   return (
     <div className='form-group'>
         <form action="" onSubmit={getStuInfo}>
             <label htmlFor="name" className='form-label'>Name</label>
-            <input type="text" name="name" id=""  className='form-control' value={name} onChange={(e)=>setName(e.target.value)} />
+            <input type="text" name="name" id=""  className='form-control' ref={nameInputRef}  />
 
             <label htmlFor="email"className='form-label'>Email</label>
-            <input type="email" name="email" id="" className='form-control' value={email} onChange={(e)=>setEmail(e.target.value)} />
+            <input type="email" name="email" id="" className='form-control' ref={emailInputRef} />
 
             <label htmlFor="live" className='form-label'>Live</label>
-            <input type="text" name="live" id="" className='form-control' value={live} onChange={(e)=>setLive(e.target.value)} />
+            <input type="text" name="live" id="" className='form-control'  ref={liveInputRef} />
 
             <input type="submit" value="Add New Student" className='btn btn-primary' />
         </form>
