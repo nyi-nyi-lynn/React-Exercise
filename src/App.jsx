63829a1fa253392931a9a50ff2eac2203f70181d
nyi-nyi-lynn <nyinyilynn163@gmail.com>
2024-10-26 13:39:10 +0630

    counter app with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import {  useState } from "react";
+import {  useReducer} from "react";
 import "./App.css";
+ const countReducer = (state,action)=>{
+    switch(action.type){
+      case "plus" : 
+         return {...state , count:state.count++,};
+      case "minus" :
+        return {...state, count:state.count--};
+        case "updateText" :
+          return {...state,text:action.payload};
+      default:
+         throw new  Error();
+    }
+ };
 
 function App() {
-  const [num, setNum] = useState(0);
-
+  const [state,dispatch] = useReducer(countReducer,{count:0,text:""});
   return (
-    <>
+    <div className="card">
       <h1>Number Count React App</h1>
+      <input type="text" name="" id="" onChange={(e)=>dispatch({type: "updateText" , payload:e.target.value})} />
+        <p>your text is {state.text}</p>
       <div className="container">
-        <button onClick={
-          (e)=>{
-            setNum((prev)=>prev+1)
-          }
-        }>+</button>
-        <span>{num}</span>
-        <button className="minus" onClick={(e)=>{
-          setNum((prev)=>prev -1)
-        }}>-</button>
+        <button onClick={(e)=>{dispatch({type:"plus"})}}>+</button>
+        <span>{state.count}</span>
+        <button className="minus" onClick={(e)=>{dispatch({type:"minus"})}}>-</button>
       </div>
-    </>
+    </div>
   );
 }
 
